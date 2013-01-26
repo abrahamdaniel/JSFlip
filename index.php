5110860d8c8b4705c6cb78fccd1dc802e858872c
Abraham Daniel <abraham@twag.co>
2013-01-26 22:25:18 +0530

    Intial

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Marshmallow Chan's postcard from Pink TV,  on Nov 29, 2011</title>
+<script src="jquery_min.js" type="text/javascript"></script>
+<script src="main.js" type="text/javascript"></script>
+<link href="main.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<body class='ie9 ie8 ie7 ie6'>
+
+<div class='container_wide'>
+<div class='container_inner'>
+<div class='clear'>
+        <div class='pc_holder'>
+        
+            <!-- / front -->
+            <div class='pc pc_front'>
+                <div class='dog_ear' style="display:block;"></div>
+                <a href="/postcards/4ed4c32e55c4750003000ec2">
+                    <img alt="Full_4ed4c32e55c4750003000ec2" class="pc_img_full" src="AppleTV-A.jpg" />
+                </a>
+                <img alt="Pc_shadow_large" class="clear" src="http://d24qy85i57kh1o.cloudfront.net/images/pc_shadow_large.png" />
+            </div>
+            <!-- / back -->
+            <div class='pc pc_back hidden'>
+                <div class='dog_ear' style="display:block;"></div>
+                
+                <table class='pc_back_full'>
+                    <tr>
+                    	<td class='pc_back_left'>"Courage is the discovery that you may not win, and trying when you know you can lose." - Steve Jobs</td>
+                        <td class='pc_back_right' valign='top'>
+                        <div class='stamp stamp_small right'><img alt="Small_postcard" src="avatar_69.jpg" /></div>
+                        <div class='pc_back_location'>
+                        <a href="/places/pink-tv">Apple, </a>
+                        <a href="http://maps.google.com/?q=44.810729,20.463631" target="_blank">
+                        <img alt="Staticmap?size=320x250&amp;markers=color:red|44.810729,20" src="http://maps.googleapis.com/maps/api/staticmap?size=320x250&amp;markers=color:red|37.327344,-122.021532&amp;sensor=false&amp;zoom=11" />
+                        </a>
+                        </div>
+                        </td>
+                    </tr>
+                </table>
+                <img alt="Pc_shadow_large" class="pc_shadow clear" src="http://d24qy85i57kh1o.cloudfront.net/images/pc_shadow_large.png" />
+            </div>
+        
+        
+        </div>
+
+</div>
+
+</div>
+
+</div>
+
+
+</body>
+
+
+</html>
